1220f3f67dbcdb1b06a6e230cd0ac3f96576d2f5
iamavu <imailavantika@gmail.com>
2024-01-02 11:59:29 +0530

    added ghidra and pwndbg

--- dragon-setup.sh
+++ dragon-setup.sh
@@ -3,7 +3,7 @@
 #zsh script to set up kali in desired state, run as kali user
 
 mkdir -p $HOME/Hack/{ctf/{pico,random},htb,portswigger,dependencies/{go,openvpn}}
-sudo apt install -y golang micro python3-venv libpcap-dev massdns python3-pwntools
+sudo apt install -y golang micro python3-venv libpcap-dev massdns python3-pwntools ghidra
 cd $HOME/Hack/dependencies && python3 -m venv py3env 
 echo "export GOPATH=$HOME/Hack/dependencies/go
 export PATH=$PATH:$GOPATH/bin
@@ -13,5 +13,6 @@ go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest
 pdtm -ia
 source $HOME/.zshrc
 go install -v github.com/tomnomnom/unfurl@latest
+cd /opt && sudo git clone https://github.com/pwndbg/pwndbg.git && cd pwndbg && sudo ./setup.sh && echo "source /opt/pwndbg/gdbinit.py" > $HOME/.gdbinit
 
 
