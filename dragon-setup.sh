2e8c9748802cf677dfea2648fcb11cc5262736f5
iamavu <imailavantika@gmail.com>
2024-01-10 20:10:51 +0530

    added new functions in zshrc and ntp

--- dragon-setup.sh
+++ dragon-setup.sh
@@ -3,11 +3,23 @@
 #zsh script to set up kali in desired state, run as kali user
 
 mkdir -p $HOME/Hack/{ctf/{pico,random},htb,portswigger,dependencies/{go,openvpn}}
-sudo apt install -y golang micro python3-venv libpcap-dev massdns python3-pwntools ghidra seclists
+sudo apt install -y golang micro python3-venv libpcap-dev massdns ntp python3-pwntools ghidra seclists
 cd $HOME/Hack/dependencies && python3 -m venv py3env 
-echo "export GOPATH=$HOME/Hack/dependencies/go
-export PATH=$PATH:$GOPATH/bin
-alias py3env='source /home/kali/Hack/dependencies/py3env/bin/activate'" >> $HOME/.zshrc
+echo "
+\n
+export GOPATH=\$HOME/Hack/dependencies/go
+export PATH=\$PATH:\$GOPATH/bin
+alias py3env='source /home/kali/Hack/dependencies/py3env/bin/activate'
+junk() 
+{
+    mv \$1 '/home/kali/.trash'
+}
+
+insgcc()
+{
+    gcc \$1 -fno-stack-protector -z execstack -no-pie && mv a.out \${1%??}
+
+}" >> $HOME/.zshrc
 source $HOME/.zshrc
 go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest
 pdtm -ia
