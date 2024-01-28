e23106892dd6747de939321c27d57fffb7112d9f
iamavu <imailavantika@gmail.com>
2024-01-28 06:22:04 +0530

    added docker install

--- dragon-setup.sh
+++ dragon-setup.sh
@@ -2,9 +2,17 @@
 
 #zsh script to set up kali in desired state, run as kali user
 
+#make directories
 mkdir -p $HOME/hack/{ctf/{pico,random},htb,blogs,portswigger,dependencies/{go,openvpn}}
-sudo apt install -y golang micro python3-venv libpcap-dev massdns ntp python3-pwntools ghidra seclists dirsearch rizin-cutter dtrx alacarte jq
+
+#install crucial stuff
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl golang micro python3-venv libpcap-dev massdns ntp python3-pwntools ghidra seclists dirsearch rizin-cutter dtrx alacarte jq
+
+#create virtual python3 environment
 cd $HOME/hack/dependencies && python3 -m venv py3env 
+
+#edit zshrc with needed stuff
 echo "
 \n
 export GOPATH=\$HOME/hack/dependencies/go
@@ -23,10 +31,25 @@ insgcc()
 
 }" >> $HOME/.zshrc
 source $HOME/.zshrc
+
+#install project discovery tools
 go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest
 pdtm -ia
 source $HOME/.zshrc
+
+#install unfurl
 go install -v github.com/tomnomnom/unfurl@latest
+
+#install pwndbg
 cd /opt && sudo git clone https://github.com/pwndbg/pwndbg.git && cd pwndbg && sudo ./setup.sh && echo "source /opt/pwndbg/gdbinit.py" > $HOME/.gdbinit
 
-
+#install docker
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  mantic stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
