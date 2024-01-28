44737754fb657a1f98961e92255f7980f5561f60
iamavu <imailavantika@gmail.com>
2024-01-28 06:29:44 +0530

    messed up and removed docker

--- dragon-setup.sh
+++ dragon-setup.sh
@@ -43,13 +43,3 @@ go install -v github.com/tomnomnom/unfurl@latest
 #install pwndbg
 cd /opt && sudo git clone https://github.com/pwndbg/pwndbg.git && cd pwndbg && sudo ./setup.sh && echo "source /opt/pwndbg/gdbinit.py" > $HOME/.gdbinit
 
-#install docker
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  mantic stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
